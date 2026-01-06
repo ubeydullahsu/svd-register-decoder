2fc630dc69fa360add33382b64687408726c9f4d
derya kara 🌱 <itsddkara@gmail.com>
2026-01-06 21:07:50 +0300

    Update gdb_plugin.py

---
+++ src/gdb_plugin.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+gdb_plugin.py
+GDB plugin to read and write memory-mapped registers using parsed SVD data.
+Author: ubeydullahsu
+
+Created: 2026-01-06
+"""
