7d4287b95f61e0063e44452dbe0828ca0f3b4642
derya kara 🌱 <itsddkara@gmail.com>
2026-01-08 22:55:37 +0300

    Update gdb_plugin.py

--- src/gdb_plugin.py
+++ src/gdb_plugin.py
@@ -9,3 +9,20 @@ Author: ubeydullahsu
 
 Created: 2025-01-06
 """
+
+import gdb
+import os
+import sys
+
+from parser import parse_svd
+from decoder import decode_register_value, format_output
+
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+class SVDPeekCommand(gdb.Command):
+    """GDB command decoder using SVD files. Usage: svd_peek <address>"""
+
+    def __init__(self):
+        super(SVDPeekCommand, self).__init__("svd_peek", gdb.COMMAND_USER)
+        self.memory_map = None # Lazy loading of memory map
